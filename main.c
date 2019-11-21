44b84ebebfcf626dfdfb0a9d1fc690611db69fd8
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-11-21 15:19:50 +0900

    practice 05

--- main.c
+++ main.c
@@ -3,10 +3,24 @@
 #include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(void) {
-	char str[30] = "happy C programming" ;
-	printf("문자열 \”%s\”의 길이 : %i", str, strlen(str));
+int main(int argc, char *argv[]) {
+	
+	int i;
+	char input[100];
+	FILE *fp;
+	
+	fp = fopen("sample.txt", "w");
+	for (i=0;i<3;i++)
+	{
+		printf("input a word:");
+		scanf("%s", input);
+		fprintf(fp, "%s\n", input);
+	}
+	
+	fclose(fp);
+	return 0;
 }
+
 
 
 
