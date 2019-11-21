a550fe5b3ebaa0635480db4695123d596d630eea
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-11-21 15:21:49 +0900

    practice 05(2)

--- main.c
+++ main.c
@@ -9,12 +9,13 @@ int main(int argc, char *argv[]) {
 	char input[100];
 	FILE *fp;
 	
-	fp = fopen("sample.txt", "w");
+	fp = fopen("sample.txt", "r");
 	for (i=0;i<3;i++)
 	{
-		printf("input a word:");
-		scanf("%s", input);
-		fprintf(fp, "%s\n", input);
+		//printf("input a word:");
+		//scanf("%s", input);
+		fscanf(fp, "%s", input);
+		printf("%s\n", input);
 	}
 	
 	fclose(fp);
